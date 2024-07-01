e0668dafbaefc7951626f8cb1668b2f2bb4c8781
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-01 07:47:49 +0530

    Added: `Press-on-mobile-app/src/navigation/AppNavigator.js`

---
+++ Press-on-mobile-app/src/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import ProjectListScreen from '../screens/ProjectListScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="ProjectList" component={ProjectListScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
